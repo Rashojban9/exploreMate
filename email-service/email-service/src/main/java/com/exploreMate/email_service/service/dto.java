09d56595efd598f9bdcb2907151165ba7c1ee4f7
Rashoj Ban <rashojban9@gmail.com>
2026-02-15 23:43:37 +0545

    email service is fix to working stage

---
+++ email-service/email-service/src/main/java/com/exploreMate/email_service/service/dto.java
@@ -0,0 +1,12 @@
+package com.exploreMate.email_service.service;
+
+import lombok.Data;
+
+@Data
+public class dto {
+
+        private String toEmail;
+        private String subject;
+        private String body;
+
+}
