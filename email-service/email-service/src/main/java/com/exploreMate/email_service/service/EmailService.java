f9b273b3c37efe1d6972237ef78cef5d9db24c09
Rashojban9 <rashojban9@gmail.com>
2026-02-16 10:34:55 +0545

    Email service is modify

--- email-service/email-service/src/main/java/com/exploreMate/email_service/service/EmailService.java
+++ email-service/email-service/src/main/java/com/exploreMate/email_service/service/EmailService.java
@@ -1,8 +1,12 @@
 package com.exploreMate.email_service.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,11 +14,22 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     private final JavaMailSender mailSender;
     public void sendBookingEmail(String toEmail,String subject,String body){
-        SimpleMailMessage message=new SimpleMailMessage();
-        message.setFrom("exploremate13@gmail.com");
-        message.setTo(toEmail);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setFrom("exploremate13@gmail.com");
+            helper.setTo(toEmail);
+            helper.setSubject(subject);
+
+
+            helper.setText(body, true);
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+
+            throw new RuntimeException("Failed to send HTML email", e);
+        }
     }
 }
